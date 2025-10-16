acda54550f1dd7363b20b597adf8a845bf531043
simka <mcj.simka@gmail.com>
2025-10-16 11:21:03 +0200

    reverse sort order of comments

--- src/utils.ts
+++ src/utils.ts
@@ -1,7 +1,7 @@
 import type { Comment } from "./types";
 
 export function sortCommentsByDate(a: Comment, b: Comment) {
-  return new Date(a.timestamp).getTime() - new Date(b.timestamp).getTime();
+  return new Date(b.timestamp).getTime() - new Date(a.timestamp).getTime();
 }
 
 export function sortNestedCommentsRecursively(comment: Comment) {
